01d30c4f1278bd40577b7f53ef0b8726a9e72244
didacpe <didac3141592@outlook.com>
2021-01-05 09:02:41 +0100

    answer not showing in console anymore

--- JS_BEGINNERS/57. Hangman App/scripts/app.js
+++ JS_BEGINNERS/57. Hangman App/scripts/app.js
@@ -4,7 +4,6 @@ const startGame = async () => {
     const MOVIE = await getMovie();
     puzzle = MOVIE.toLowerCase().split("");
     setGame(puzzle);
-    console.log(puzzle);
 };
 
 window.addEventListener('keypress', playGame);
